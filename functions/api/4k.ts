465c39cf52e422ece5ec58c7293aab0925bb6d04
millerhuaers <millerhuaers185@gmail.com>
2026-04-17 16:39:29 +0800

    Update 4k.ts

--- functions/api/4k.ts
+++ functions/api/4k.ts
@@ -1,35 +1,14 @@
-import { getLangFromHeader } from "./utils";
+import { getBingWallpaperUrl } from "./utils";
 
 export const onRequest: PagesFunction = async (context) => {
-	try {
-		const lang = getLangFromHeader(
-			context.request.headers.get("accept-language")
-		);
-		const url = new URL(context.request.url);
-		const index = url.searchParams.get("index") || "0";
+	const url = new URL(context.request.url);
+	const idx = url.searchParams.get("index") || "0";
 
-		const response = await fetch(`https://www.bing.com/HPImageArchive.aspx?format=js&idx=${index}&n=1&mkt=${lang}`);
-		if (!response.ok) throw new Error("Bing API error");
-		const data: any = await response.json();
-
-		if (data?.images?.[0]) {
-			// 拼接 4K (UHD) 分辨率图片链接
-			const imageUrl = `https://www.bing.com${data.images[0].urlbase}_UHD.jpg`;
-			
-			const headers = new Headers();
-			headers.set("Access-Control-Allow-Origin", "*");
-			headers.set("Cache-Control", "max-age=3600");
-			headers.set("Location", imageUrl);
-
-			return new Response(null, {
-				status: 302,
-				headers: headers
-			});
+	return await getBingWallpaperUrl(
+		context.request.headers.get("accept-language"),
+		idx,
+		(imgUrl) => {
+			return imgUrl.replace("1920x1080", "UHD");
 		}
-		throw new Error("No image found");
-	} catch {
-		const headers = new Headers();
-		headers.set("Access-Control-Allow-Origin", "*");
-		return new Response(null, { headers, status: 500 });
-	}
+	);
 };
